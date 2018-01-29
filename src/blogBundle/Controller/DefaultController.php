b3d4f3aed43bcbb3136f76c9200ce0afa2eac63b
Thomas Cristophol <thomas.cristophol@agencemayflower.com>
2018-01-29 15:41:54 +0100

    safe symfony

---
+++ src/blogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace blogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('blogBundle:Default:index.html.twig');
+    }
+}
