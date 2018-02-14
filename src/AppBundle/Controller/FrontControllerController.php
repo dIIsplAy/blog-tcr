41051fe93a7d09699eafe0e2da2dbe34af1b37b7
Thomas Cristophol <thomas.cristophol@agencemayflower.com>
2018-02-14 15:46:57 +0100

    fix add coment to article

--- src/AppBundle/Controller/FrontControllerController.php
+++ src/AppBundle/Controller/FrontControllerController.php
@@ -43,26 +43,27 @@ class FrontControllerController extends Controller
     
     }
 
-    public function newAction(Article $article ,Request $request)
+    public function newAction(Article $article, Request $request)
     {   
         $comment = new Comment();
         $comment->setArticle($article);
+
         $form = $this->createForm('AppBundle\Form\CommentType', $comment);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($comment);
-            $em->flush();
-            unset($entity);
-            
-
-            
+            $em->flush();   
         }
-        return $this->render('AppBundle:FrontController:new.html.twig', array(
-            'comment' => $comment,
-            'form' => $form->createView(),
+
+        return $this->redirectToRoute('show', array(
+            'id' => $article->getId()
         ));
+        // return $this->render('AppBundle:FrontController:show.html.twig', array(
+        //     'comment' => $comment,
+        //     'form' => $form->createView(),
+        // ));
     }
 
 }
